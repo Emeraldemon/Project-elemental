9085c1f8cb3099f29cf3212cbf65a349649ce9b1
Emeraldemon <61049348+Emeraldemon@users.noreply.github.com>
2020-08-13 21:16:30 +0600

    Add files via upload

---
+++ Elemental version_1.0.py
@@ -0,0 +1,240 @@
+#project elemo
+a = int(input('enter atomic number of the element'))
+if a == 1:
+        print('hydrogen')
+elif a == 2:
+        print('helium')
+elif a == 3:
+        print('lithium')
+elif a == 4:
+        print('beryllium')
+elif a == 5:
+    print('boron')
+elif a == 6:
+    print('carbon')
+elif a == 8:
+    print('oxygen')
+elif a == 9:
+    print('fluorine')
+elif a == 7:
+    print('nitrogen')
+elif a == 10:
+    print('neon')
+elif a == 11:
+    print('sodium')
+elif a == 12:
+    print('magnesium')
+elif a == 13:
+    print('aluminium')
+elif a == 14:
+    print('silicon')
+elif a == 15:
+    print('phosphorus')
+elif a == 16:
+    print('sulphur')
+elif a == 17:
+    print('chlorine')
+elif a == 18:
+    print('argon')
+elif a == 19:
+    print('potassium')
+elif a == 20:
+    print('calcium')
+elif a == 21:
+    print('scandium')
+elif a == 22:
+    print('titanium')
+elif a == 23:
+    print('vanadium')
+elif a == 24:
+    print('chromium')
+elif a == 25:
+    print('manganese')
+elif a == 26:
+    print('iron')
+elif a == 27:
+    print('cobalt')
+elif a == 28:
+    print('nickel')
+elif a == 29:
+    print('copper')
+elif a == 30:
+    print('zinc')
+elif a == 31:
+    print('gallium')
+elif a == 32:
+    print('germanium')
+elif a == 33:
+    print('arsenic')
+elif a == 34:
+    print('selenium')
+elif a == 35:
+    print('bromine')
+elif a == 36:
+    print('krypton')
+elif a == 37:
+    print('rubidium')
+elif a == 38:
+    print('strontium')
+elif a == 39:
+    print('yttrium')
+elif a == 40:
+    print('zirconium')
+elif a == 41:
+    print('niobium')
+elif a == 42:
+    print('molybdenum')
+elif a == 43:
+    print('technetium')
+elif a == 44:
+    print('ruthenium')
+elif a == 45:
+    print('rhodium')
+elif a == 46:
+    print('palladium')
+elif a == 47:
+    print('silver')
+elif a == 48:
+    print('cadmium')
+elif a == 49:
+    print('indium')
+elif a == 50:
+    print('tin')
+elif a == 51:
+    print('antimony')
+elif a == 52:
+    print('tellurium')
+elif a == 53:
+    print('iodine')
+elif a == 54:
+    print('xenon')
+elif a == 55:
+    print('cesium')
+elif a == 56:
+    print('barium')
+elif a == 57:
+    print('lanthanum')
+elif a == 58:
+    print('cerium')
+elif a == 59:
+    print('praseodymium')
+elif a == 60:
+    print('neodymium')
+elif a == 61:
+    print('promethium')
+elif a == 62:
+    print('samarium')
+elif a == 63:
+    print('europium')
+elif a == 64:
+    print('gadolinium')
+elif a == 65:
+    print('terbium')
+elif a == 66:
+    print('dysprosium')
+elif a == 67:
+    print('holmium')
+elif a == 68:
+    print('erbium')
+elif a == 69:
+    print('thulium')
+elif a == 70:
+    print('ytterbium')
+elif a == 71:
+    print('lutetium')
+elif a == 72:
+    print('halfnium')
+elif a == 73:
+    print('tantalum')
+elif a == 74:
+    print('tungsten')
+elif a == 75:
+    print('rhenium')
+elif  a == 76:
+    print('osmium')
+elif a == 77:
+    print('iridium')
+elif a == 78:
+    print('platinum')
+elif a == 79:
+    print('gold')
+elif a == 80:
+    print('Hydrargyrum')
+elif a == 81:
+    print('thallium')
+elif a == 82:
+    print('lead')
+elif a == 83:
+    print('bismuth')
+elif a == 84:
+    print('polonium')
+elif a == 85:
+    print('astatine')
+elif a == 86:
+    print('radon')
+elif a == 87:
+    print('francium')
+elif a == 88:
+    print('radium')
+elif a == 89:
+    print('actinium')
+elif a == 90:
+    print('thorium')
+elif a == 91:
+    print('protactinium')
+elif a == 92:
+    print('uranium')
+elif a == 93:
+    print('neptunium')
+elif a == 94:
+    print('plutonium')
+elif a == 95:
+    print('americium')
+elif a == 96:
+    print('curium')
+elif a == 97:
+    print('berkelium')
+elif a == 98:
+    print('californium')
+elif a == 99:
+    print('einsteinium')
+elif a == 100:
+    print('fermium')
+elif a == 101:
+    print('mendelevium')
+elif a == 102:
+    print('nobelium')
+elif a == 103:
+    print('lawrencium')
+elif a == 104:
+    print('rutherfordium')
+elif a == 105:
+    print('dubnium')
+elif a == 106:
+    print('seaborgium')
+elif a == 107:
+    print('bohrium')
+elif a == 108:
+    print('hassium')
+elif a == 109:
+    print('meitnerium')
+elif a == 110:
+    print('darmstadtium')
+elif a == 111:
+    print('roentgenium')
+elif a == 112:
+    print('copernicium')
+elif a == 113:
+    print('ununtrium')
+elif a == 114:
+    print('flerovium')
+elif a == 115:
+    print('ununpentium')
+elif a == 116:
+    print('livermorium')
+elif a == 117:
+    print('ununseptium')
+elif a == 118:
+    print('ununoctium')
+#project elemo by vampire labs
+#finished on 29 july,monday,5:07 p.m.(IST)
